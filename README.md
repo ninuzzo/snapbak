0fcb53ff30d2a7d4e65846b0e74ef6d99e2c872a
Antonio Bonifati <antonio.bonifati@gmail.com>
2013-04-10 18:41:15 +0200

    First import

--- README.md
+++ README.md
@@ -2,3 +2,30 @@ snapbak
 =======
 
 LVM-based virtual machines backup with rsync
+
+What it is
+-------
+
+It is a companion to [my selective backup kit - bak](http://ninuzzo.freehostia.com/sw/bak.html),
+see -  for those who have LVM-based KVM virtual machines and want to back them
+up using rsync (e.g. to an external cheap USB disk).
+
+`snapbak.sh` can call `bak` to backup the host configuration and copy it to the
+external drive, but that is optional. If you want to use `bak`, just uncomment
+and edit the `backup_host_config` function in `backup.conf`.
+
+Installation instruction
+-------
+
+Copy the configuration file into `/usr/local/etc/snapbak.conf` and the script
+into `/usr/local/sbin/snapbak.sh` Edit the configuration file and change the
+mount point `BAKMNT`, the list of VMs to backup from the default `(vm1 vm2)`
+and set `VG` to the volume group the VMs are in. By default the latter is the
+same as the short host name.
+
+Depending on how much activity there is on you VMs during the backup, you may
+want to adjust the size of snapshots. This is a percentage of each VM size as
+set by `SNAPSIZE`, by default 10, which means 10%.
+
+If you need other features to be implemented, please let me know. If you
+implement a new feature by yourself, it would be nice if you contribute it.
---
+++ snapbak.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Back up the host configuration and LVM-based virtual machines,
+# by rsyncing on an external diskdrive.
+#
+# Antonio Bonifati <antonio.bonifati@gmail.com>
+
+# Source the config file, first of all.
+CONF=/usr/local/etc/snapbak.conf
+source $CONF || exit
+
+# Make sure the USB disk is mounted!
+grep "$BAKMNT" /proc/mounts || mount "$BAKMNT" || exit
+
+# Optional selective host configuration backup.
+[ "$(type -t backup_host_config)" = "function" ] && backup_host_config
+
+# Snapshots a VM and mounts the snapshot readonly (e.g. for backup purposes).
+# $1 - vm name
+# Creates global variables SNAPNAME and SNAPVOL
+create_snapshot() {
+  # Tries to make up a unique snapshot name for a VM.
+  SNAPNAME="$1snap$(date +%s)"
+
+  SNAPVOL="/dev/$VG/$SNAPNAME"
+
+  local lvpath="$VG"/"$1" \
+    # Compute the snapshot LV size in megabytes (m) as a percentage of the VM virtual disk size.
+    lvsize=$(echo "$(lvs --noheadings --units m -o lv_size "$lvpath" | sed 's/m$//') * $SNAPSIZE / 100" | bc -l)
+
+  lvcreate -s -n "$SNAPNAME" -L ${lvsize}m "$lvpath" &&
+
+  # Expose the partitions within the snapshot.
+  kpartx -a "$SNAPVOL" &&
+
+  # Assume the first partition is the one to backup.
+  mount -t auto -o ro "/dev/mapper/$VG-${SNAPNAME}p1" "$SNAPMNT"
+}
+
+# Unmount and destroy the last created VM snapshot.
+destroy_snapshot() {
+  umount "$SNAPMNT" &&
+  kpartx -d "$SNAPVOL" &&
+  lvremove -f "$SNAPVOL"
+}
+
+# Update a VM backup.
+# $1 - vm name
+update_backup() {
+  local destdir="$BAKMNT/$1/"
+
+  # Make sure the destination dir exists
+  mkdir "$destdir" &&
+
+  rsync -aq --delete "$SNAPMNT/" "$BAKMNT/$1/"
+}
+
+# Create a temporary mount point to use for snapshots.
+SNAPMNT=$(mktemp -d)
+
+# Online VM backup, using LVM snapshots.
+for vm in "${VMS[@]}"; do
+  create_snapshot "$vm" &&
+  update_backup "$vm" &&
+  destroy_snapshot "$vm"
+done
+
+rmdir $SNAPMNT
+
+# Unmount the disk, just in case someone pulls it out.
+umount "$BAKMNT"
